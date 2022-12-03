12f415c74b29cd5a7399867370707c828378cd80
AleFerri01pd <ferri.alessandro2001@gmail.com>
2022-12-03 12:26:48 +0100

    Get Proprieties from Contract

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import "./App.css";
 import React from "react";
 import lottery from "./lottery";
+import web3 from "./web3";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { manager: '' };
+  state = { //dichiarizioni direttamente incluse nel costruttore (ES6)
+    manager: '',
+    players: [],
+    balance: '',
   }
 
   async componentDidMount() { //funzionate nativa che viene chiamata al render del component
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    console.log(lottery);
-
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
 
   render() {    
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>The manager of this Contract is {this.state.manager}</p>
+        <p>
+          Il Manager del contratto è {this.state.manager}.
+          Ci sono al momento {this.state.players.length} persone che participano alla lotteria.
+          Competi per vincere {web3.utils.fromWei(this.state.balance, 'ether')} ether!!
+        </p>
       </div>
     );
   }
